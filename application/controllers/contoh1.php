b0a3bc6ff600949eb3cafecc2be763ed8c31a57e
sfrjnh37 <safira.jannah16@gmail.com>
2023-10-13 13:04:11 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "nama saya Safiratul Jannah
+            saya tinggal di daerah Banyuannyar
+            olah raga yang saya sukai adalah 
+            Badminton";
+    }
+}
