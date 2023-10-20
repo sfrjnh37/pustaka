0c1c3deeea3c7e9fa96f4725baa27613a293a7d8
Safiratul Jannah <145754137+sfrjnh37@users.noreply.github.com>
2023-10-20 13:27:28 +0700

    Add files via upload

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Safiratul</li>
+ <li>Nama Belakang : Jannah</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan bone timur 2 banyuanyar</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>surakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>Catur</li>
+ </ul>
+</section>
